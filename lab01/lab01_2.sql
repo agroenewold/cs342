69412c394e539c554c04f147b1b7f8a1b0ec08cf
agroenewold <groenewold.andrew@gmail.com>
2017-02-10 12:50:08 -0500

    Delete lab01_2.sql

--- lab01/lab01_2.sql
+++
@@ -1,9 +0,0 @@
---Andrew Groenewold
---CS342, lab01, ex1.2
-
---dual has one column, DUMMY, that is a string of length one. It has one value, 'X'
---It can be joined to another table to double the amount of rows in that table.
---It can also be used to make sure the database is running.
-
-select * from dual;
-describe dual;
